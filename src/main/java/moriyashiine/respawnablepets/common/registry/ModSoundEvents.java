126316ee5273acf48020a0054cf77c894e9fe967
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-04-14 02:49:05 +0000

    Update ModSoundEvents.java

--- src/main/java/moriyashiine/respawnablepets/common/registry/ModSoundEvents.java
+++ src/main/java/moriyashiine/respawnablepets/common/registry/ModSoundEvents.java
@@ -13,6 +13,6 @@ public class ModSoundEvents {
 	public static final SoundEvent ENTITY_GENERIC_TELEPORT = new SoundEvent(new Identifier(RespawnablePets.MOD_ID, "entity.generic.teleport"));
 
 	public static void init() {
-		Registry.register(Registry.SOUND_EVENT, new Identifier(RespawnablePets.MOD_ID, "entity.generic.teleport"), ENTITY_GENERIC_TELEPORT);
+		Registry.register(Registry.SOUND_EVENT, ENTITY_GENERIC_TELEPORT.getId(), ENTITY_GENERIC_TELEPORT);
 	}
 }
