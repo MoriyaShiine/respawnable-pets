af660d777fe87b6fc9e0940c1e8006d1efd336b0
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-12-13 15:57:01 -0600

    add slider to config

--- src/main/java/moriyashiine/respawnablepets/common/ModConfig.java
+++ src/main/java/moriyashiine/respawnablepets/common/ModConfig.java
@@ -7,6 +7,6 @@ package moriyashiine.respawnablepets.common;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ModConfig extends MidnightConfig {
-	@Entry(min = -1, max = 23999)
+	@Entry(min = -1, max = 23999, isSlider = true)
 	public static int timeToRespawn = -1;
 }
